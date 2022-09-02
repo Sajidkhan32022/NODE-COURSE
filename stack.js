d36519a2681fd182ccdfc08db8b2d3e1a1a6dc17
sajid Khan <sajidkhan320222gmail.com>
2022-09-01 23:46:53 -0700

    worked on settimeout function and stack

---
+++ stack.js
@@ -0,0 +1,13 @@
+console.log('starting')
+setTimeout(()=>{
+    console.log('------')
+},0)
+setTimeout(()=>{
+    console.log('2000s')
+},2000);
+
+setTimeout(() => {
+  console.log('0s')  
+}, 0);
+
+console.log('ending')
